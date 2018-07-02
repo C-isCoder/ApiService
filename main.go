9e86e000ed433fe25592adffd13da0ae25720619
orzlllll@qq.com <congxiaodan>
2018-07-02 14:40:35 +0800

    demo 2

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"apiservice/config"
 	"apiservice/router"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfg = pflag.StringP("config", "c", "", "apiservice config file path")
 )
 
 func main() {
+	pflag.Parse()
+
+	// init config
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	// Set gin mode.
+	gin.SetMode(viper.GetString("runmode"))
+
 	// 创建路由引擎
 	g := gin.New()
 	middlewares := []gin.HandlerFunc{}
@@ -18,6 +35,29 @@ func main() {
 		middlewares...,
 	)
 
-	log.Printf("Strat to listening the incoming requests on http addresss: %s", ":8989")
-	log.Printf(http.ListenAndServe(":8989", g).Error())
+	// Ping the server to make sure the router is working
+	go func(){
+		if err : = pingServer(); err != nil{
+			log.Fatal("The router has no response, or it might took too long to start up.",err)
+		}
+	}()
+
+	log.Printf("Strat to listening the incoming requests on http address: %s", viper.GetString("port"))
+	log.Printf(http.ListenAndServe(viper.GetString("port"), g).Error())
+}
+
+// pingServer pings the http server to make sure the router is working
+func pingServer() error{
+	for i := 0;i<viper.GetInt("max_ping_count"); i++{
+		// Ping the server by sending a GET request to '/helath'.
+		resp, err := http.Get(viper.GetString("url")+"/sd/health")
+		if err == nil && resp.StatusCode == 200{
+			return nil
+		}
+
+		// Sleep for a second to continue the next ping.
+		log.Print("Wating for the router, retry in 1 second.")
+		time.Sleep(thime.Second)
+	}
+	return errors.New("Cannot connect to the router.")
 }
