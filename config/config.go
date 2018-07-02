906f389fd012d20abed394fb8b9188f75e01375a
orzlllll@qq.com <congxiaodan>
2018-07-02 16:25:42 +0800

    fix bug

--- config/config.go
+++ config/config.go
@@ -4,8 +4,8 @@ import (
 	"strings"
 
 	"github.com/fsnotify/fsnotify"
-	"github.com/spf13/viper"
 	"github.com/lexkong/log"
+	"github.com/spf13/viper"
 )
 
 type Config struct {
@@ -62,13 +62,13 @@ func (c *Config) watchConfig() {
 func (c *Config) initLog() {
 	passLagerCfg := log.PassLagerCfg{
 		Writers:        viper.GetString("log.writers"),
-		LoggerLevel:    viper.GetString("log.logger_level")
-		LoggerFile:     viper.GetString("log.logger_file")
-		LogFormatText:  viper.GetBool("log.log_format_text")
-		RollingPolicy:  viper.GetString("log.rollingPolicy")
-		LogRotateDate:  viper.GetInt("log.log_rotate_date")
-		LogRotateSize:  viper.GetInt("log.log_rotate_size")
-		LogBackupCount: viper.GetInt("log.log_backup_count")
+		LoggerLevel:    viper.GetString("log.logger_level"),
+		LoggerFile:     viper.GetString("log.logger_file"),
+		LogFormatText:  viper.GetBool("log.log_format_text"),
+		RollingPolicy:  viper.GetString("log.rollingPolicy"),
+		LogRotateDate:  viper.GetInt("log.log_rotate_date"),
+		LogRotateSize:  viper.GetInt("log.log_rotate_size"),
+		LogBackupCount: viper.GetInt("log.log_backup_count"),
 	}
 	log.InitWithConfig(&passLagerCfg)
 }
