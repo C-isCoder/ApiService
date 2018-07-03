ae5b3f9a2d4c29a080ed2cc89d78595219dc846b
orzlllll@qq.com <congxiaodan>
2018-07-03 11:37:19 +0800

    fix bug

--- pkg/errno/code.go
+++ pkg/errno/code.go
@@ -4,7 +4,7 @@ var (
 	// Common errors
 	OK                  = &Errno{Code: 0, Message: "OK"}
 	InternalServerError = &Errno{Code: 10001, Message: "Internal server error"}
-	ErrBind             = &Errno{Code: 20102, Message: "The user was not found."}
+	ErrBind             = &Errno{Code: 10002, Message: "Error occurred while binding the request body to the struct."}
 
 	// user errors
 	ErrUserNotFount = &Errno{Code: 20102, Message: "The user was not found."}
