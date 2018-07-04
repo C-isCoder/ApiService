7cf59d280b5e340503c73d1285d37c75c4921f93
orzlllll@qq.com <congxiaodan>
2018-07-04 17:38:12 +0800

    fix bug

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -4,7 +4,7 @@ import "golang.org/x/crypto/bcrypt"
 
 // Encrypt encrypts the plain text with bcrypt.
 func Encrypt(source string) (string, error) {
-	hashedBytes, err := bcrypt.GenerateFromPasswrd([]byte(source), bcrypt.DefaultCost)
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(source), bcrypt.DefaultCost)
 	return string(hashedBytes), err
 }
 
