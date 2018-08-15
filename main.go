f98dc3098b35d2e8f1122a9967127391739ac63e
orzlllll@qq.com <congxiaodan>
2018-08-15 17:55:47 +0800

    demo 12

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"errors"
 	"net/http"
 	"time"
+	"encoding/json"
+	"fmt"
+	"os"
 
 	"apiservice/config"
 	"apiservice/model"
 	"apiservice/router"
-
 	"apiservice/router/middleware"
+	v "apiservice/pkg/version"
+
 	"github.com/gin-gonic/gin"
 	"github.com/lexkong/log"
 	"github.com/spf13/pflag"
@@ -17,12 +21,23 @@ import (
 )
 
 var (
-	cfg = pflag.StringP("config", "c", "", "apiservice config file path")
+	cfg     = pflag.StringP("config", "c", "", "apiservice config file path")
+	version = pflag.BoolP("version", "v", false, "show version info.")
 )
 
 func main() {
 	pflag.Parse()
+	if *version {
+		info := v.Get
+		marshalled, err := json.MarshalIndent(&info, "", " ")
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
 
+		fmt.Println(string(marshalled))
+		return
+	}
 	// init config
 	if err := config.Init(*cfg); err != nil {
 		panic(err)
