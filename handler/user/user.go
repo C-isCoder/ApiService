65fc84310f9688efed11745e283d7f008bc1320e
orzlllll@qq.com <congxiaodan>
2018-08-20 17:55:34 +0800

    fix swagger

--- handler/user/user.go
+++ handler/user/user.go
@@ -21,3 +21,8 @@ type ListResponse struct {
 	TotalCount uint64            `json:"totalCount"`
 	UserList   []*model.UserInfo `json:"userlist"`
 }
+
+type SwaggerListResponse struct {
+	TotalCount uint64           `json:"totalCount"`
+	UserList   []model.UserInfo `json:"userList"`
+}
